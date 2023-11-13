163145736b05d12d3947020c4eef8a9ef6b7d293
shakilkhan496 <shakilkhan496@gmail.com>
2023-11-13 21:31:59 +0600

    Update index.js

--- index.js
+++ index.js
@@ -364,7 +364,7 @@ app.post('/webhook', express.raw({ type: 'application/json' }), async (request, 
                 const existingData = matchingRecord.fields['Last successful payment receipt URL (for stipe)'] || '';
                 const newDataValue = invoicePaymentIntentSucceed.hosted_invoice_url;
                 // Append the new data to the existing data
-                const newData = `Pagamento efetuado em: ${convertUnixTimestampToDate(invoicePaymentIntentSucceed.status_transitions.paid_at)} ${newDataValue} \n ${existingData}`;
+                const newData = `Pagamento efetuado em: ${convertUnixTimestampToDate(invoicePaymentIntentSucceed.status_transitions.paid_at)} ${newDataValue} \n\n ${existingData}`;
 
 
                 if (matchingRecord) {
