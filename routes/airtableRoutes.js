e87ae68d17489131283e404f84477746943528b9
condoroo <150356902+condoroo@users.noreply.github.com>
2024-01-08 12:11:06 +0000

    commented phcinvoiceroutes & airtableroutes

--- routes/airtableRoutes.js
+++ routes/airtableRoutes.js
@@ -1,33 +1,43 @@
-const express = require('express');
+// Import the express module and create a router object
+const express = require("express");
 const router = express.Router();
 
-const { default: axios } = require('axios');
+// Import axios, a promise-based HTTP client, for making requests
+const { default: axios } = require("axios");
 
-
-// AirTable api keys
+// Retrieve Airtable API configuration from environment variables
 const AIRTABLE_API_KEY = `${process.env.AIRTABLE_API_KEY}`;
 const AIRTABLE_BASE_ID = `${process.env.AIRTABLE_BASE_ID}`;
 const AIRTABLE_TABLE_NAME = `${process.env.AIRTABLE_TABLE_NAME}`;
 
-router.get('/get-records', async (req, res) => {
-    try {
-        const airtableURL = `https://api.airtable.com/v0/${AIRTABLE_BASE_ID}/${AIRTABLE_TABLE_NAME}`;
-        const response = await axios.get(airtableURL, {
-            headers: {
-                Authorization: `Bearer ${AIRTABLE_API_KEY}`,
-            },
-        });
-        const records = response.data.records;
-        // console.log(records);
-        const specificRecord = records.find(record => record.id === 'recvbfR2l1O0zQxVT');
+// Define a GET route '/get-records' to handle client requests for Airtable records
+router.get("/get-records", async (req, res) => {
+  try {
+    // Construct the Airtable endpoint URL using the base ID and table name
+    const airtableURL = `https://api.airtable.com/v0/${AIRTABLE_BASE_ID}/${AIRTABLE_TABLE_NAME}`;
+    // Perform a GET request to the Airtable API
+    const response = await axios.get(airtableURL, {
+      headers: {
+        Authorization: `Bearer ${AIRTABLE_API_KEY}`, // Include the Airtable API key in the request header for authentication
+      },
+    });
+    // Extract the records from the Airtable response
+    const records = response.data.records;
 
-        res.send(specificRecord);
-        // res.send(records);
+    // Find a specific record by its ID, 'recvbfR2l1O0zQxVT'
+    const specificRecord = records.find(
+      (record) => record.id === "recvbfR2l1O0zQxVT"
+    );
 
-    } catch (error) {
-        console.error('Error fetching Airtable records:', error);
-        res.status(500).json({ error: 'Unable to fetch records' });
-    }
+    // Send the specific record as the response to the client
+    res.send(specificRecord);
+  } catch (error) {
+    // Log any errors that occur during the fetching of records
+    console.error("Error fetching Airtable records:", error);
+    // Respond with a 500 Internal Server Error status code and an error message
+    res.status(500).json({ error: "Unable to fetch records" });
+  }
 });
 
+// Export the router for use in other parts of the application
 module.exports = router;
--- routes/phcInvoiceRoutes.js
+++ routes/phcInvoiceRoutes.js
@@ -1,246 +1,302 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+const { default: axios } = require("axios");
 
-const { default: axios } = require('axios');
+// Endpoint paths for generating an access token, creating a document & creating a customer
+const createCustomerEndpoint = "/createCustomer";
+const generateAccessTokenEndpoint = "/generateAccessToken";
+const createDocumentEndpoint = "/createDocument";
 
-const createCustomerEndpoint = '/createCustomer';
+/**
+ * Route handler to create a customer.
+ * Expects body to have name, email, tax_number, observations, backendUrl, appId, userCode,
+ * password, company, tokenLifeTime, API_URL.
+ * @param {object} req - The request object from the client.
+ * @param {object} res - The response object to send the data back to the client.
+ */
+router.post("/createCustomer", async (req, res) => {
+  // Extract the body parameters sent with the request
+  const {
+    name,
+    email,
+    tax_number,
+    observations,
+    backendUrl,
+    appId,
+    userCode,
+    password,
+    company,
+    tokenLifeTime,
+    API_URL,
+  } = req.body;
 
-router.post('/createCustomer', async (req, res) => {
-    const generateAccessTokenEndpoint = '/generateAccessToken';
-    const { name, email, tax_number, observations,
-        backendUrl,
-        appId,
-        userCode,
-        password,
-        company,
-        tokenLifeTime,
-        API_URL,
-    } = req.body;
-
-    /////////functions
-    /*************************************************************
- *          Function to request access token                 *
- *************************************************************/
-    async function requestAccessToken() {
-        const url = API_URL + generateAccessTokenEndpoint;
-
-        const credentials = {
-            backendUrl,
-            appId,
-            userCode,
-            password,
-            company,
-            tokenLifeTime,
-        };
-
-        try {
-            console.log('Requesting access token with credentials:', credentials);
-            const response = await axios.post(url, { credentials });
-            console.log('Access token response (headers):', response.headers);
-            console.log('Access token response (status):', response.status);
-            console.log('Access token response (data):', response.data);
-
-            if (response.data.code === 100) {
-                console.error('Error in login:', response.data.message);
-                throw new Error('Error in login. Please verify your credentials.');
-            }
-            return response.data.token;
-        } catch (error) {
-            console.error('Error in access token request:', error.response?.data || error.message);
-            throw error;
-        }
-    }
-
-
-
-    /*************************************************************
-     *          Function to create a customer                    *
-     *************************************************************/
-    async function createCustomer(token, customerData) {
-        const url = API_URL + createCustomerEndpoint;
-
-        try {
-            console.log('Creating customer with token:', token);
-            console.log('Customer data:', customerData);
-
-            const response = await axios.post(url, customerData, {
-                headers: {
-                    'Content-Type': 'application/json',
-                    Authorization: token,
-                },
-            });
-
-            console.log('Create customer response:', response.data);
-
-            if (response.data.code === 100) {
-                throw new Error('Error creating customer. Server returned code 100.');
-            }
-
-            return response.data;
-        } catch (error) {
-            console.error('Error creating customer:', error.response?.data || error.message);
-            throw error;
-        }
-    }
-
-    //////////
-    try {
-        // #1 - First, get an access token
-        const accessToken = await requestAccessToken();
-        console.log(accessToken);
-        if (!accessToken) {
-            throw new Error('Error generating access token');
-        }
-
-        console.log('Access Token Generated Successfully!');
-
-        // #2 - Basic data to create the necessary customer
-        const customerData = {
-            customer: {
-                name: name,
-                address: '',
-                postalCode: '',
-                city: '',
-                country: 'Pt',
-                email: email,
-                taxNumber: tax_number,
-                phone: '',
-                mobilePhone: '',
-                iban: '',
-                bic: '',
-                observations: observations,
-            },
-        };
-
-        // Make the request to create a customer
-        const customerCreationData = await createCustomer(accessToken, customerData);
-
-        res.json(customerCreationData);
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ code: 100, message: 'Internal Server Error' });
-    }
-});
-
-router.post('/createInvoice', async (req, res) => {
-    const generateAccessTokenEndpoint = '/generateAccessToken';
-    const createDocumentEndpoint = '/createDocument';
-    const { name, email, tax_number, observations,
-        backendUrl,
-        appId,
-        userCode,
-        password,
-        company,
-        tokenLifeTime,
-        API_URL,
-        designation,
-        unitPrice,
-        number
-    } = req.body;
-    console.log('this is tax number', tax_number);
-    /////////functions
-    /*************************************************************
- *          Function to request access token                 *
- *************************************************************/
-    async function requestAccessToken() {
-        const url = API_URL + generateAccessTokenEndpoint;
-
-        const credentials = {
-            backendUrl,
-            appId,
-            userCode,
-            password,
-            company,
-            tokenLifeTime,
-        };
-
-        try {
-            console.log('Requesting access token with credentials:', credentials);
-            const response = await axios.post(url, { credentials });
-            console.log('Access token response (headers):', response.headers);
-            console.log('Access token response (status):', response.status);
-            console.log('Access token response (data):', response.data);
-
-            if (response.data.code === 100) {
-                console.error('Error in login:', response.data.message);
-                throw new Error('Error in login. Please verify your credentials.');
-            }
-            return response.data.token;
-        } catch (error) {
-            console.error('Error in access token request:', error.response?.data || error.message);
-            throw error;
-        }
-    }
-    async function createDocument(token, documentData) {
-        const url = API_URL + createDocumentEndpoint;
-
-        try {
-            const response = await axios.post(url, documentData, {
-                headers: {
-                    'Content-Type': 'application/json',
-                    Authorization: token,
-                },
-            });
-
-            console.log('Create document response:', response.data);
-
-            if (response.data.code === 100) {
-                console.error('Error creating document:', response.data.message);
-                throw new Error('Error creating document');
-            }
-
-            return response.data;
-        } catch (error) {
-            console.error('Error creating document:', error.response?.data || error.message);
-            throw error;
-        }
-    }
+  /**
+   * Requests an access token from the API.
+   * @returns {Promise<string>} The access token string.
+   */
+  async function requestAccessToken() {
+    const url = API_URL + "/generateAccessToken";
+    const credentials = {
+      backendUrl,
+      appId,
+      userCode,
+      password,
+      company,
+      tokenLifeTime,
+    };
 
     try {
-        // #1 - First, get an access token
-        const accessToken = await requestAccessToken();
-        console.log('Access Token Generated Successfully!');
+      // Log the credentials for debugging (might be sensitive - ensure logging is secure)
+      console.log("Requesting access token with credentials:", credentials);
+      // Make a post request to the API to get an access token
+      const response = await axios.post(url, { credentials });
+      // Log the response for debugging
+      console.log("Access token response:", response);
 
-        // #2 - Create a document
-        const documentData = {
-            customer: {
-                number: number,
-                name,
-                taxNumber: `${tax_number}`,
-            },
-            requestOptions: {
-                option: 1,
-                requestedFields: [],
-            },
-            document: {
-                docType: 1,
-                invoicingAddress1: '',
-                invoicingPostalCode: '',
-                invoicingLocality: '',
-                documentObservations: observations,
-            },
-            products: [
-                {
-                    reference: '',
-                    designation: designation,
-                    unitCode: 'M',
-                    unitPrice: unitPrice,
-                    quantity: 1,
-                    taxIncluded: true,
-                    taxPercentage: 23,
-                    taxRegion: 'PT',
-                }
-            ],
-        };
+      // Check if the API responded with an error code
+      if (response.data.code === 100) {
+        console.error("Error in login:", response.data.message);
+        throw new Error("Error in login. Please verify your credentials.");
+      }
 
-        const creationMessage = await createDocument(accessToken, documentData);
-        console.log(creationMessage);
-
-        res.json(creationMessage);
+      // Return the access token from the response
+      return response.data.token;
     } catch (error) {
-        console.error(error);
-        res.status(500).json({ code: 100, message: 'Internal Server Error' });
+      console.error(
+        "Error in access token request:",
+        error.response?.data || error.message
+      );
+      throw error;
     }
+  }
+
+  /**
+   * Creates a customer using the provided token and customer data.
+   * @param {string} token - The access token for API authorization.
+   * @param {object} customerData - The customer data to be sent to the API.
+   * @returns {Promise<object>} The response data from the API.
+   */
+  async function createCustomer(token, customerData) {
+    const url = API_URL + createCustomerEndpoint;
+
+    try {
+      console.log("Creating customer with token:", token);
+      console.log("Customer data:", customerData);
+
+      // Make a post request to create the customer with the given data
+      const response = await axios.post(url, customerData, {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: token,
+        },
+      });
+
+      console.log("Create customer response:", response.data);
+
+      // Check if the API responded with an error code
+      if (response.data.code === 100) {
+        throw new Error("Error creating customer. Server returned code 100.");
+      }
+
+      // Return the API response
+      return response.data;
+    } catch (error) {
+      console.error(
+        "Error creating customer:",
+        error.response?.data || error.message
+      );
+      throw error;
+    }
+  }
+
+  // Try to get an access token, create a customer, and send back the API response
+  try {
+    const accessToken = await requestAccessToken();
+    console.log("Access Token Generated Successfully!");
+
+    // Set up the customer data structure
+    const customerData = {
+      customer: {
+        name: name,
+        address: "",
+        postalCode: "",
+        city: "",
+        country: "Pt",
+        email: email,
+        taxNumber: tax_number,
+        phone: "",
+        mobilePhone: "",
+        iban: "",
+        bic: "",
+        observations: observations,
+      },
+    };
+
+    // Create the customer using the access token and customer data
+    const customerCreationData = await createCustomer(
+      accessToken,
+      customerData
+    );
+
+    // Send back the successful response from the API
+    res.json(customerCreationData);
+  } catch (error) {
+    // Log the error and send a 500 Internal Server Error response
+    console.error(error);
+    res.status(500).json({ code: 100, message: "Internal Server Error" });
+  }
 });
 
+// POST route handler for creating an invoice
+router.post("/createInvoice", async (req, res) => {
+  // Destructure and assign variables from the request body
+  const {
+    name,
+    email,
+    tax_number,
+    observations,
+    backendUrl,
+    appId,
+    userCode,
+    password,
+    company,
+    tokenLifeTime,
+    API_URL,
+    designation,
+    unitPrice,
+    number,
+  } = req.body;
+
+  // Log the tax number for debugging
+  console.log("this is tax number", tax_number);
+
+  // Function to request an access token from the remote API
+  async function requestAccessToken() {
+    // Construct the full URL for the token generation endpoint
+    const url = API_URL + generateAccessTokenEndpoint;
+
+    // Package credentials for the token request
+    const credentials = {
+      backendUrl,
+      appId,
+      userCode,
+      password,
+      company,
+      tokenLifeTime,
+    };
+
+    try {
+      // Make a POST request to the API to get an access token
+      const response = await axios.post(url, { credentials });
+
+      // Log various parts of the response for debugging
+      console.log("Access token response (headers):", response.headers);
+      console.log("Access token response (status):", response.status);
+      console.log("Access token response (data):", response.data);
+
+      // Check if the response contains an error code and throw an error if so
+      if (response.data.code === 100) {
+        console.error("Error in login:", response.data.message);
+        throw new Error("Error in login. Please verify your credentials.");
+      }
+
+      // Return the access token
+      return response.data.token;
+    } catch (error) {
+      // Log any errors and rethrow them
+      console.error(
+        "Error in access token request:",
+        error.response?.data || error.message
+      );
+      throw error;
+    }
+  }
+
+  // Function to create a document (invoice) using the provided access token and data
+  async function createDocument(token, documentData) {
+    // Construct the URL for creating the document
+    const url = API_URL + createDocumentEndpoint;
+
+    try {
+      // Make a POST request to the API to create the document with the access token in the header
+      const response = await axios.post(url, documentData, {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: token,
+        },
+      });
+
+      // Log the response for debugging
+      console.log("Create document response:", response.data);
+
+      // Check if the response contains an error code and throw an error if so
+      if (response.data.code === 100) {
+        console.error("Error creating document:", response.data.message);
+        throw new Error("Error creating document");
+      }
+
+      // Return the response data
+      return response.data;
+    } catch (error) {
+      // Log any errors and rethrow them
+      console.error(
+        "Error creating document:",
+        error.response?.data || error.message
+      );
+      throw error;
+    }
+  }
+
+  try {
+    // Request an access token
+    const accessToken = await requestAccessToken();
+    console.log("Access Token Generated Successfully!");
+
+    // Define the data structure for the document to be created
+    const documentData = {
+      customer: {
+        number: number,
+        name,
+        taxNumber: `${tax_number}`,
+      },
+      requestOptions: {
+        option: 1,
+        requestedFields: [],
+      },
+      document: {
+        docType: 1,
+        invoicingAddress1: "",
+        invoicingPostalCode: "",
+        invoicingLocality: "",
+        documentObservations: observations,
+      },
+      products: [
+        {
+          reference: "",
+          designation: designation,
+          unitCode: "M",
+          unitPrice: unitPrice,
+          quantity: 1,
+          taxIncluded: true,
+          taxPercentage: 23,
+          taxRegion: "PT",
+        },
+      ],
+    };
+
+    // Create the document with the access token and document data
+    const creationMessage = await createDocument(accessToken, documentData);
+    console.log(creationMessage);
+
+    // Send the response from the document creation back to the client
+    res.json(creationMessage);
+  } catch (error) {
+    // Log any errors that occur during the process
+    console.error(error);
+    // Send a 500 Internal Server Error response back to the client
+    res.status(500).json({ code: 100, message: "Internal Server Error" });
+  }
+});
+
+// Export the router for use in other parts of the application
 module.exports = router;
